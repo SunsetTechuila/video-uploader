ba6775d4ea351a66c6e4dcbf38f4efdcc21934bf
Grigory <techuila.sunset@gmail.com>
2024-04-10 19:52:47 +0500

    refactor(video-uploader/app/modules/processVideo/saveVideoFrame): remove unnecessary `async`

--- packages/video-uploader/src/app/modules/processVideo/saveVideoFrame.ts
+++ packages/video-uploader/src/app/modules/processVideo/saveVideoFrame.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import-x/no-named-as-default
 import FfmpegCommand from "fluent-ffmpeg";
 
-export default async function saveVideoFrame(
+export default function saveVideoFrame(
   path: string,
   outFile: string,
   time = "00:00:00",
